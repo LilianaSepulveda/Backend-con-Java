73e36aff56308ec937a7221ecb3c38605b37c0b0
lilisep <lilianasepulveda13@gmail.com>
2024-10-16 16:33:29 -0500

    carpeta intro java

---
+++ Intro a Java/Act. Tipo de Dato Adecuado/SeleccionarDatos.java
@@ -0,0 +1,7 @@
+public class SeleccionarDatos {
+    public static void main(String[] args) {
+        //determinar el tipo de dato más adecuado en Java para representar cada uno:
+        
+    }
+    
+}
