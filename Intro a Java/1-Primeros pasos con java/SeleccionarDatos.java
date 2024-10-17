9289c1b5cc66650b496c69f1a661eaff44f09e56
lilisep <lilianasepulveda13@gmail.com>
2024-10-17 12:47:35 -0500

    data types

--- Intro a Java/1-Primeros pasos con java/SeleccionarDatos.java
+++ Intro a Java/1-Primeros pasos con java/SeleccionarDatos.java
@@ -1,7 +1,26 @@
 public class SeleccionarDatos {
     public static void main(String[] args) {
-        //determinar el tipo de dato más adecuado en Java para representar cada uno:
-        
+        // determinar el tipo de dato más adecuado en Java para representar cada uno:
+        // 1-La temperatura media de una ciudad durante el verano (en grados Celsius).
+        double temp;
+        // 2-El precio de un producto en una tienda (en dólares y centavos).
+        double price;
+        // 3-El nombre completo de una persona.
+        String name;
+        // 4-La fecha de nacimiento de una persona.
+        String date;
+        // 5-El número de habitantes de una ciudad.
+        long population;
+        // 6-La duración de una película (en minutos).
+        float minutes;
+        // 7-Si un artículo está disponible o no en una tienda (true/false).
+        boolean article;
+        // 8-La distancia entre dos ciudades (en kilómetros).
+        double distance;
+        // 9-El número de teléfono de una persona.
+        int phoneNumber;
+        // 10-La cantidad de existencias de un producto en una tienda.
+        int product;
     }
-    
+
 }
