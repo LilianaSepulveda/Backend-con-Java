bf1a671e4c50c56fc599cd1ad5834280f3b06ce0
lilisep <lilianasepulveda13@gmail.com>
2024-10-21 15:05:39 -0500

    update looking element

--- Intro a Java/10-Arrays/BuscarElemento.java
+++ Intro a Java/10-Arrays/BuscarElemento.java
@@ -1,3 +1,4 @@
+
 // El programa debe crear un array de tamaño 3 que almacene números enteros. Luego, solicita un número al usuario 
 //y muestra por consola si ese número está presente en el array o no. Es importante utilizar únicamente 
 //las herramientas aprendidas hasta el momento, sin emplear bucles en el proceso.
@@ -5,31 +6,28 @@ import java.util.Scanner;
 
 public class BuscarElemento {
     public static void main(String[] args) {
-        int[] number = {4,7,9};
+        int[] number = { 4, 7, 9 };
 
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Ingresa un número para buscar: ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\033\143");// para quitar en consola el texto relleno
+        System.out.println("Ingresa un número para buscar: ");
 
-    int looking = sc.nextInt();
+        int looking = sc.nextInt();
         boolean existe = false;
-        if(number[0] == looking){
+        if (number[0] == looking) {
             existe = true;
         }
-        if (number[1] == looking){
+        if (number[1] == looking) {
             existe = true;
         }
-        if (number[2] == looking){
+        if (number[2] == looking) {
             existe = true;
         }
+        if (existe) {
+            System.out.println("El número " + looking + " buscado existe en el array");
+        }
 
-
-
-    //System.out.println("el número "+looking+);
+        sc.close();
     }
-    
-    
-   
 
-    
-    
 }
